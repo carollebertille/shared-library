88d8ac47fa711d38e4c45371aac0eca95d5648ea
carollebertille <99866372+carollebertille@users.noreply.github.com>
2024-01-09 20:36:52 -0800

    Create pythonCheck.groovy

---
+++ vars/pythonCheck.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+// check python syntax
+def call(){
+  
+   sh 'sudo yum install epel-release -y'
+   sh 'sudo yum install python-pip -y'
+   sh 'pip --version'
+   sh 'pip install pylint -y'
+   sh 'pylint  \${WORKSPACE}/source_code/module/database.py'
+
+}
