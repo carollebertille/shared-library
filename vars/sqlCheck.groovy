c06f38713dc462d79fc81a804c493faa4c1a2e15
carollebertille <99866372+carollebertille@users.noreply.github.com>
2024-01-09 20:37:35 -0800

    Create sqlCheck.groovy

---
+++ vars/sqlCheck.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+
+// check bach syntax
+def call(){
+    sh 'sudo curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -'
+    sh 'sudo yum install nodejs -y'
+    sh 'sudo npm install -g sql-lint -y'
+    sh 'sql-lint  \${WORKSPACE}/database/crud_flask.sql'
+
+
+}
