8cbfa64e45724b9d5c03be0faa723897e91faa64
carollebertille <99866372+carollebertille@users.noreply.github.com>
2024-01-09 20:42:52 -0800

    Create golangCheck.groovy

---
+++ vars/golangCheck.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+// check bach syntax
+def call(){
+    sh 'sudo yum install golang -y'
+    sh 'go get -u golang.org/x/lint/golint'
+    sh 'export PATH="$PATH:/home/ubuntu/go/bin/"'
+    sh 'golint  \${WORKSPACE}/fake-backend/main.go'
+    sh 'golint  \${WORKSPACE}/fake-backend/config.go'
+
+
+}
