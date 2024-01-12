6d64fd6dd25f6fc2d8b816e8240e901c42c53e75
carollebertille <99866372+carollebertille@users.noreply.github.com>
2024-01-12 14:39:53 -0600

    Create bashCheck.groovy

---
+++ vars/bashCheck.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+
+// check bach syntax
+def call(){
+
+    sh 'shellcheck --version'
+    sh 'apk  --no-cache add grep'
+    sh '''
+    for file in $(grep -IRl "#!(/usr/bin/env |/bin/)" --exclude-dir ".git" --exclude Jenkinsfile \${WORKSPACE}); do
+      if ! shellcheck -x $file; then
+        export FAILED=1
+      else
+        echo "$file OK"
+      fi
+    done
+    if [ "${FAILED}" = "1" ]; then
+      exit 1
+    fi
+    '''
+
+}
