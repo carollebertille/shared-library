9e6488611bba82f683459d44f39a83eec58235b8
carollebertille <99866372+carollebertille@users.noreply.github.com>
2024-01-12 20:08:05 -0800

    Update slacknotifier.groovy

--- vars/slacknotifier.groovy
+++ vars/slacknotifier.groovy
@@ -2,15 +2,15 @@
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "CONGRATULATIONS: Job ${env.JOB_NAME} with build number ${env.BUILD_NUMBER} was successful! Triggered by ${BUILD_USER}. More info: ${env.BUILD_URL}"
+    slackSend color: "good", message: "CONGRATULATIONS: Job ${env.JOB_NAME} with build number ${env.BUILD_NUMBER} was successful! . More info: ${env.BUILD_URL}"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed ! Triggered by ${BUILD_USER}. more info ${env.BUILD_URL}"
+    slackSend color: "danger", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed ! . more info ${env.BUILD_URL}"
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable ! Triggered by ${BUILD_USER}. more info ${env.BUILD_URL}"
+    slackSend color: "warning", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable ! . more info ${env.BUILD_URL}"
   }
   else {
-    slackSend color: "danger", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its result was unclear ! Triggered by ${BUILD_USER}. more info ${env.BUILD_URL}"	
+    slackSend color: "danger", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its result was unclear ! . more info ${env.BUILD_URL}"	
   }
 }
